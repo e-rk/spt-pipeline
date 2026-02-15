756a8f837d58e3fd74d934d87b02490c2002a7b4
Rafał Kuźnia <rafal.kuznia@protonmail.com>
2026-02-15 02:56:54 +0100

    car: import interior and export interior camera

--- src/spt_pipeline/resources/car2gltf.py
+++ src/spt_pipeline/resources/car2gltf.py
@@ -17,19 +17,21 @@ try:
     parser.add_argument("-o", "--output")
     args = parser.parse_args(argv[1:])
     bpy.ops.wm.read_factory_settings(use_empty=True)
-    # bpy.ops.preferences.addon_enable(module="io_nfs4_import")
-    bpy.ops.preferences.addon_enable(module="bl_ext.user_default.speedtools")
+    bpy.ops.preferences.addon_enable(module="io_nfs4_import")
+    # bpy.ops.preferences.addon_enable(module="bl_ext.user_default.speedtools")
     bpy.ops.preferences.addon_enable(module="io_scene_gltf2")
     bpy.ops.import_scene.nfs4car(
         directory=args.input,
         import_lights=True,
         import_audio=True,
+        import_interior=True,
     )
     bpy.ops.export_scene.gltf(
         filepath=args.output,
         export_attributes=True,
         export_extras=True,
         export_lights=True,
+        export_cameras=True,
     )
 except:
     exit(1)
